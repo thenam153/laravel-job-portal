d4ac6e565757285636638b51d1e1a971be741be2
thenam153 <noilinhtinh016@gmail.com>
2019-11-25 21:52:44 +0700

    update config & add send mail

---
+++ app/Mail/ConfirmUser.php.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ConfirmUser.php extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($code, $name)
+    {
+        //
+        $this->code= $code;
+        $this->name= $name;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('admin@job.com')
+                ->view('mail.confirmCode',['code'=> $this->code,'name'=>$this->name]);
+    }
+}
---
+++ resources/views/mail/confirmCode.blade.php
@@ -0,0 +1,17 @@
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+<div class="container" style="width: 60%; display: block; margin: 50px 20%;	position: absolute;	border: 3px solid #2f3542; font-family: 'Source Sans Pro', sans-serif;">
+	<div class="header" style="position: relative; widows: 80%; text-align: center; color: #fff; font-size: 32px; background-color: #16a085; border-bottom: 3px solid #2f3542; padding: 10px;">
+		Thư xác nhận từ Phone Sale
+	</div>
+	<div class="content" style="padding: 30px; background-color: #ecf0f1; text-align: justify; font-size: 16px;">
+		<p>Chào bạn, đây là thư xác nhận đăng kí tài khoản từ Phone Sale <a href="{{URL::to('confirmuser')}}/{{$code}}">click vào đây để xác nhận</a></p>
+		<p>Nếu bạn không đăng kí tài khoản, vui lòng không click vào đường dẫn trên và phản hồi lại với chúng tối qua Email.</p>
+		<p>Rất cảm ơn quý khách!</p>
+		<table width="50%">
+			<tr style="width: 40%">
+				<td style="color: red; font-weight: bold;">Tên khách truy cập: </td>
+				<td>{{$name}}</td>
+			</tr>
+		</table>
+	</div>
+</div>

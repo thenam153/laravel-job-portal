feb48f7580bcbd0ff6c25948de700477a599b4ad
thenam153 <noilinhtinh016@gmail.com>
2019-12-12 11:25:48 +0700

    updateheroku

--- resources/views/product/modules/banner-category.blade.php
+++ resources/views/product/modules/banner-category.blade.php
@@ -5,9 +5,9 @@
         <div class="row d-flex align-items-center justify-content-center">
             <div class="about-content col-lg-12">
                 <h1 class="text-white">
-                    Job category				
+                    Tìm việc				
                 </h1>	
-                <p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="category.html"> Job category</a></p>
+                <p class="text-white link-nav"><a href="index">Trang chủ </a>  <span class="lnr lnr-arrow-right"></span>  <a href="category"> theo danh mục</a></p>
             </div>											
         </div>
     </div>

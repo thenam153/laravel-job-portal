d81b8bc8dcb77d077f5c98f244193afaf55a2dfb
ThiinhUET <thinhlevan2015@gmail.com>
2019-12-19 09:13:03 +0000

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+	    //$this->call(UsersTableSeeder::class);
+	    for($i = 0; $i < 20 ; $i++) {
+		    DB::table('users')->insert([
+			    'name' => Str::random(10),
+			    'email' => 'admin_job_'.rand(0, 100).'@job.com',
+			    'password' => bcrypt('123456'),
+			    'phone' => '0989878776',
+			    'level' => 1
+		    ]);
+	    }
     }
 }
